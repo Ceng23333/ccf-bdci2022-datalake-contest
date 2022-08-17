2977d91a9cf8e3d73df617679ce6b0909c8b215f
zenghua <441651826@qq.com>
2022-08-17 10:37:15 +0800

    [read_test]add debug script for read-test

---
+++ rundebug_read.sh
@@ -0,0 +1,2 @@
+sudo rm -rf /home/huazeng/test/result
+docker run --net lakesoul-compose-env_default --rm -it -v /home/huazeng/ccf-bdci2022-datalake-contest-examples/lakesoul/target/jars:/opt/spark/extra_jars  -v /home/huazeng/test:/home/huazeng/test --env lakesoul_home=/opt/spark/extra_jars/lakesoul.properties swr.cn-north-4.myhuaweicloud.com/dmetasoul-repo/spark:v3.1.2 spark-shell --driver-class-path "/opt/spark/extra_jars/*" --class org.ccf.bdci2022.datalake_contest.Read /opt/spark/extra_jars/datalake_contest.jar --localtest
