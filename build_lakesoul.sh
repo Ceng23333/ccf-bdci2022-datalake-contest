7a813c0cf1b11391b9ac6f18e223da35beb6ba3a
zenghua <441651826@qq.com>
2022-08-19 15:04:05 +0800

    update debug scripts

--- build_lakesoul.sh
+++ build_lakesoul.sh
@@ -13,7 +13,7 @@ mkdir -p lakesoul/target/jars
 
 cp lakesoul/target/datalake_contest-1.0.0-SNAPSHOT.jar lakesoul/target/jars/datalake_contest.jar
 cp lakesoul/lakesoul.properties lakesoul/target/jars
-cp $(LakeSoulLib)/liblakesoul_io_c.so lakesoul/target/jars
+cp $LakeSoulLib/liblakesoul_io_c.so lakesoul/target/jars
 
 # mvn dependency:copy-dependencies -DoutputDirectory=target/jars -DincludeScope=runtime -DexcludeGroupIds=org.slf4j,org.apache.logging.log4j -pl lakesoul -DskipTests
 
