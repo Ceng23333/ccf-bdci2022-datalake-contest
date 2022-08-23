f9b26cdd4ce4120118685f98afdea23ae637455b
zenghua <441651826@qq.com>
2022-08-23 16:39:13 +0800

    read batchcontent without partition

--- lakesoul/src/main/scala/org/ccf/bdci2022/datalake_contest/Read.scala
+++ lakesoul/src/main/scala/org/ccf/bdci2022/datalake_contest/Read.scala
@@ -37,8 +37,8 @@ object Read {
     val tablePath= "/home/huazeng/test/table/table_test"
 //    val df = LakeSoulTable.forPath(tablePath).toDF
     val df = spark.read.format("lakesoul").load(tablePath)
-    df.selectExpr("id","first_name","last_name","email","ip_address","cc","country","birthdate","salary","title","comments" ).show(100)
-//      df.selectExpr("gender" ).show(100)
+//    df.selectExpr("id","first_name","last_name","email","ip_address","cc","country","birthdate","salary","title","comments" ).show(100)
+      df.selectExpr("gender" ).show(100)
 //    df.write.parquet("/home/huazeng/test/result")
   }
 }
