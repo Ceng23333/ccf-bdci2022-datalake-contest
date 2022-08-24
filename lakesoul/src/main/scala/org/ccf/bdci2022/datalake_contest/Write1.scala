69533d8bebe3c064fb4b7daffa9da2481cc5ea23
zenghua <441651826@qq.com>
2022-08-24 16:29:16 +0800

    add test case

--- lakesoul/src/main/scala/org/ccf/bdci2022/datalake_contest/Write1.scala
+++ lakesoul/src/main/scala/org/ccf/bdci2022/datalake_contest/Write1.scala
@@ -70,7 +70,8 @@ object Write1 {
       when(df2("first_name").isNotNull && df2("first_name").notEqual("null"), df2("first_name")).otherwise(df1("first_name")).alias("first_name"),
       when(df2("country").isNotNull, df2("country")).otherwise(df1("country")).alias("country"),
       when(df2("email").isNotNull, df2("email")).otherwise(df1("email")).alias("email")
-    ).write.mode("Overwrite").format("lakesoul").save(tablePath)
+    ).write.mode("Overwrite").format("lakesoul")
+        .option("rangePartitions","gender").save(tablePath)
   }
 
 }
