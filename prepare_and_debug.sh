2fc45117bb7fb6c1da0e1bcf928ed0b4d005f823
zenghua <441651826@qq.com>
2022-08-29 16:27:27 +0800

    add test case

--- prepare_and_debug.sh
+++ prepare_and_debug.sh
@@ -1,5 +1,7 @@
 ls /home/huazeng/.m2/repository/com/dmetasoul
 lakesoul_spark="/home/huazeng/.m2/repository/com/dmetasoul/lakesoul-spark/2.1.0-lakesoul-spark-3.1.2-SNAPSHOT/lakesoul-spark-2.1.0-lakesoul-spark-3.1.2-SNAPSHOT.jar"
+ls $lakesoul_spark
 lakesoul_common="/home/huazeng/.m2/repository/com/dmetasoul/lakesoul-common/2.1.0-SNAPSHOT/lakesoul-common-2.1.0-SNAPSHOT.jar"
+ls $lakesoul_common
 cp $lakesoul_spark $lakesoul_common /opt/spark/jars
 spark-shell --conf spark.sql.extensions=com.dmetasoul.lakesoul.sql.LakeSoulSparkSessionExtension --conf spark.sql.catalog.spark_catalog=org.apache.spark.sql.lakesoul.catalog.LakeSoulCatalog
