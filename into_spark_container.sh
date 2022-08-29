1caa5b6a8349bdaf2abe2925cd945e7ed5483e5f
zenghua <441651826@qq.com>
2022-08-29 16:15:02 +0800

    add test case

--- into_spark_container.sh
+++ into_spark_container.sh
@@ -5,6 +5,7 @@ DIR="$(realpath "${DIR}")"
 
 cd "$DIR"
 
+rm /home/huazeng/ccf-bdci2022-datalake-contest-examples/lakesoul/target/jars/prepare_and_debug.sh
 cp prepare_and_debug.sh /home/huazeng/ccf-bdci2022-datalake-contest-examples/lakesoul/target/jars
 
-docker run --net lakesoul-compose-env_default --rm -it -v /home/huazeng/ccf-bdci2022-datalake-contest-examples/lakesoul/target/jars:/opt/spark/work-dir  -v /home/huazeng/test:/home/huazeng/test -v ~/.m2/repository/com/dmetasoul/:/home/huazeng/.m2/repository/com/dmetasoul/ --env RUST_BACKTRACE=1 --env LakeSoulLib=/opt/spark/work-dir --env lakesoul_home=/opt/spark/work-dir/lakesoul.properties swr.cn-north-4.myhuaweicloud.com/dmetasoul-repo/spark:v3.1.2 /opt/spark/work-dir/prepare_and_debug.sh
+docker run --net lakesoul-compose-env_default --rm -it -v /home/huazeng/ccf-bdci2022-datalake-contest-examples/lakesoul/target/jars:/opt/spark/work-dir  -v /home/huazeng/test:/home/huazeng/test -v ~/.m2/repository/com/dmetasoul/:/home/huazeng/.m2/repository/com/dmetasoul/ --env RUST_BACKTRACE=1 --env LakeSoulLib=/opt/spark/work-dir --env lakesoul_home=/opt/spark/work-dir/lakesoul.properties swr.cn-north-4.myhuaweicloud.com/dmetasoul-repo/spark:v3.1.2 bash /opt/spark/work-dir/prepare_and_debug.sh
